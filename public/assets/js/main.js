d8f7597358e0345d7b8a7d3f4bd45669ff3821bf
jibriloh <jibrilomar295@gmail.com>
2025-03-11 22:38:06 -0400

    initial commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,15 @@
+new Glide('.glide', {
+    type: 'carousel', 
+    autoplay: 2000, 
+    animationDuration: 800, 
+    keyboard: true,
+    hoverpause: true,
+ }).mount();
+
+ 
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'disableScrolling': true
+});
+
